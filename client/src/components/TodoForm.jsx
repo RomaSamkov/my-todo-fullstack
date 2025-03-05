374cab4a3911ffbd39fdf7734c0565d57323cad5
RomaSamkov <Romansamkov.rs@gmail.com>
2025-03-05 14:12:19 +0200

    add styles

--- client/src/components/TodoForm.jsx
+++ client/src/components/TodoForm.jsx
@@ -21,8 +21,8 @@ const TodoForm = () => {
   };
 
   return (
-    <div>
-      <form>
+    <div className="flex flex-col gap-6">
+      <form className="flex items-center gap-2">
         <label htmlFor="todo">Todo</label>
         <input
           type="text"
@@ -30,12 +30,14 @@ const TodoForm = () => {
           name="todo"
           value={todo.text}
           onChange={(e) => setTodo({ ...todo, text: e.target.value })}
+          className="border rounded-2xl"
         />
         <button onClick={addNewTodo}>Add Todo</button>
       </form>
-      <ol>
+      <ol className="flex flex-col justify-items-center border rounded-3xl p-2 gap-2">
+        <h2>Todo list:</h2>
         {todos.map((todo, index) => (
-          <li key={index}>
+          <li key={index} className="flex justify-between items-center px-2">
             <span>{todo}</span>
             <button onClick={() => dispatch(removeTodo(index))}>
               Remove Todo
