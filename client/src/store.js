d1ae7e369f2528516fcdffee6d4638d0dd91ca1c
RomaSamkov <Romansamkov.rs@gmail.com>
2025-03-05 15:12:38 +0200

    add notesReducer

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todosReducer from "./store/todoSlice";
+import notesReducer from "./store/noteSlice";
 
 export const store = configureStore({
   reducer: {
     todos: todosReducer,
+    notes: notesReducer,
   },
 });
