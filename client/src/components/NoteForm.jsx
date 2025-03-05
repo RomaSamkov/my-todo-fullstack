53e0b97ee800b4758d30d25d4e3972cfe357a42d
RomaSamkov <Romansamkov.rs@gmail.com>
2025-03-05 17:45:47 +0200

    Update NoteForm.jsx

--- client/src/components/NoteForm.jsx
+++ client/src/components/NoteForm.jsx
@@ -1,31 +1,11 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addNote, deleteNote } from "../store/noteSlice";
-import axios from "axios";
 
 const NoteForm = () => {
   const [note, setNote] = useState("");
-  const [addfetch, setAddfetch] = useState("");
-  const [fetchedNotes, setFetchedNotes] = useState([]);
   const notes = useSelector((state) => state.notes);
   const dispatch = useDispatch();
-
-  const fetchNotes = async () => {
-    const response = await axios.get("http://localhost:5000/notes");
-    setFetchedNotes(response.data);
-  };
-
-  const addFetchNote = async (e) => {
-    e.preventDefault();
-    console.log("Click");
-
-    const response = await axios.post("http://localhost:5000/add", addfetch, {
-      headers: { "Content-Type": "text/plain" },
-    });
-    console.log("🚀 ~ addFetchNote ~ response:", response.data);
-    setAddfetch("");
-    fetchNotes();
-  };
 
   const addNewNote = (e) => {
     e.preventDefault();
@@ -58,25 +38,6 @@ const NoteForm = () => {
             </li>
           ))}
         </ol>
-      </div>
-      <div>
-        <button onClick={fetchNotes}>Fetch Button</button>
-        <form className="flex items-center gap-2">
-          <label htmlFor="fetchnote">Note</label>
-          <textarea
-            id="fetchnote"
-            name="fetchnote"
-            value={addfetch}
-            onChange={(e) => setAddfetch(e.target.value)}
-            className="border rounded-2xl"
-          />
-          <button onClick={addFetchNote}>Add Note</button>
-        </form>
-        <div>
-          {fetchedNotes.map((fetchNote, index) => (
-            <li key={index}>{fetchNote}</li>
-          ))}
-        </div>
       </div>
     </div>
   );
