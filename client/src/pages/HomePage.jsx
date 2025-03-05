b892060ad9cfd36bae2add5f8259bc2c69e768d5
RomaSamkov <Romansamkov.rs@gmail.com>
2025-03-05 14:12:29 +0200

    Create HomePage.jsx

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <nav className="flex gap-4 justify-center p-4 text-3xl">
+        <Link to={"/todo"}>Todo Form</Link>
+        <Link to={"/note"}>Note Form</Link>
+      </nav>
+    </div>
+  );
+};
+
+export default HomePage;
