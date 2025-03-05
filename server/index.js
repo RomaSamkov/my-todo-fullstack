6f1a3204caf4cd0d2faf4b822da45c4042f810ac
RomaSamkov <Romansamkov.rs@gmail.com>
2025-03-05 17:37:54 +0200

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import { database } from "./database.js";
+
+const port = 5000;
+
+const app = express();
+app.use(express.text());
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
+
+app.use("/notes", (req, res) => {
+  res.send(database);
+});
+
+app.use("/add", (req, res) => {
+  const note = req.body;
+  console.log(note);
+
+  database.push(note);
+  res.send(database);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port:${port}`);
+});
