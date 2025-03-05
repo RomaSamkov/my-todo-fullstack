a4b73534e4b218b1dcd33287c6fa0e99262fc922
RomaSamkov <Romansamkov.rs@gmail.com>
2025-03-05 14:11:50 +0200

    add Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import TodoForm from "./components/TodoForm";
+import HomePage from "./pages/HomePage";
+import Logo from "./components/Logo";
+import NoteForm from "./components/NoteForm";
 
 function App() {
   return (
     <>
-      <TodoForm />
+      <Logo />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/todo" element={<TodoForm />} />
+        <Route path="note" element={<NoteForm />} />
+      </Routes>
     </>
   );
 }
