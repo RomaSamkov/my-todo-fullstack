99ffbd4e22631daeda1c4c34c24245668c2a87b0
RomaSamkov <Romansamkov.rs@gmail.com>
2025-03-06 17:40:23 +0200

    add extraReducers

--- client/src/store/noteSlice.js
+++ client/src/store/noteSlice.js
@@ -1,26 +1,60 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-const loadNotes = () => {
-  const savedTodos = localStorage.getItem("notes");
-  return savedTodos
-    ? JSON.parse(savedTodos)
-    : ["text storage 1", "Text - App", "Text - Music"];
-};
+const API_URL = "http://localhost:5000/api/notes";
+
+export const fetchNotes = createAsyncThunk("notes/fetchNotes", async () => {
+  const response = await fetch("http://localhost:5000/api/notes");
+  const data = await response.json();
+  return data;
+});
+
+export const addNote = createAsyncThunk("notes/addNote", async (note) => {
+  const response = await fetch("http://localhost:5000/api/add", {
+    method: "POST",
+    headers: { "Content-Type": "text/plain" },
+    body: note,
+  });
+  const data = await response.json();
+  return data;
+});
+
+export const deleteNote = createAsyncThunk(
+  "notes/deleteNote",
+  async (index) => {
+    const response = await fetch("http://localhost:5000/api/delete", {
+      method: "DELETE",
+      headers: { "Content-Type": "text/plain" },
+      body: index.toString(),
+    });
+    const data = await response.json();
+    return data;
+  }
+);
 
 const noteSlice = createSlice({
   name: "notes",
-  initialState: loadNotes(),
-  reducers: {
-    addNote: (state, action) => {
-      state.push(action.payload);
-      localStorage.setItem("notes", JSON.stringify(state));
-    },
-    deleteNote: (state, action) => {
-      state.splice(action.payload, 1);
-      localStorage.setItem("notes", JSON.stringify(state));
-    },
+  initialState: { items: [], status: "idle", error: null },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchNotes.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchNotes.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.items = action.payload;
+      })
+      .addCase(fetchNotes.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      })
+      .addCase(addNote.fulfilled, (state, action) => {
+        state.items = action.payload;
+      })
+      .addCase(deleteNote.fulfilled, (state, action) => {
+        state.items = action.payload;
+      });
   },
 });
 
-export const { addNote, deleteNote } = noteSlice.actions;
 export default noteSlice.reducer;
