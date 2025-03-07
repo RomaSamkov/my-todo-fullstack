6d9ade1f13381bbda7bd91beefc940bbec920fdd
RomaSamkov <Romansamkov.rs@gmail.com>
2025-03-07 17:23:22 +0200

    Create BookStore.jsx

---
+++ client/src/components/BookStore.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+const BookStore = () => {
+  const [book, setBook] = useState({ author: "", title: "" });
+  const [allBooks, setAllBooks] = useState({});
+
+  const addBook = (e) => {
+    e.preventDefault();
+    if (book.author.trim() === "" || book.title.trim() === "") return;
+    const id = Date.now().toString();
+    setAllBooks((prevBooks) => ({ ...prevBooks, [id]: book }));
+    setBook({ author: "", title: "" });
+    console.log(book);
+  };
+  return (
+    <div className="">
+      <form className="flex flex-col items-center gap-2">
+        <div className="flex gap-2">
+          <label htmlFor="note">Author:</label>
+          <input
+            id="author"
+            name="author"
+            value={book.author}
+            onChange={(e) => setBook({ ...book, author: e.target.value })}
+            className="border rounded-2xl p-2"
+          />
+        </div>
+        <div className="flex gap-2">
+          <label htmlFor="note">Title:</label>
+          <input
+            id="title"
+            name="title"
+            value={book.title}
+            onChange={(e) => setBook({ ...book, title: e.target.value })}
+            className="border rounded-2xl p-2"
+          />
+        </div>
+        <button onClick={addBook}>Add Book</button>
+      </form>
+      <ul>
+        {Object.entries(allBooks).map(([id, b]) => (
+          <li key={id}>
+            {b.author} - {b.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default BookStore;
