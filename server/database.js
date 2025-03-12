36a4c748e975561f595e4fb627b073d6f6db80a6
RomaSamkov <Romansamkov.rs@gmail.com>
2025-03-12 12:06:26 +0200

    Update database.js

--- server/database.js
+++ server/database.js
@@ -3,3 +3,8 @@ export const database = [
   "Random fake text 2",
   "Random fake text 3",
 ];
+
+export const databaseBooks = [
+  { author: "J.K. Rowling", title: "Harry Potter" },
+  { author: "J.R.R. Tolkien", title: "The Hobbit" },
+];
